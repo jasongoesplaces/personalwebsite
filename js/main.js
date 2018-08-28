81ee83ea3fd25fa743557a5c2fd0fb268035cf37
Jason <jasongoesplaces@gmail.com>
2018-08-28 18:09:51 -0400

    mobile test

--- js/main.js
+++ js/main.js
@@ -61,9 +61,5 @@ jQuery(document).ready(function($) {
     
 });
 
-$('.service').click(function(e) {
-    e.preventDefault();
-});
-
 
 TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
