adeb6b15072f29b1903967f5650eb2062de0e340
Jason <jasongoesplaces@gmail.com>
2018-09-11 22:20:00 -0400

    update navbar

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 /*----------------------------------------------------*/
-/* Quote Loop
------------------------------------------------------- */
-
-function fade($ele) {
-    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
-        var $next = $(this).next('.quote');
-        fade($next.length > 0 ? $next : $(this).parent().children().first());
-   });
-}
-fade($('.quoteLoop > .quote').first());
-
-
-/*----------------------------------------------------*/
 /* Navigation
 ------------------------------------------------------ */
 
