5f6acc5d75835806e264cf928c7261b47e8906dc
Jason <jasongoesplaces@gmail.com>
2018-09-13 17:00:11 -0400

    code cleanup

--- js/main.js
+++ js/main.js
@@ -53,4 +53,4 @@ function portLink(url) {
     win.focus();
 }
 
-TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
+// TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
